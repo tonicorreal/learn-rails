5e6ea7e4624b9f6df61a5942e07ca94c14bb5e28
Antonio Correa <tonicorreal@gmail.com>
2016-05-29 12:04:34 -0400

    contact form

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,21 @@
+class ContactsController < ApplicationController
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(secure_params)
+    if @contact.valid?
+      #TODO sned message
+      flash[:notice] = "Message sent from #{@contact.name}."
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def secure_params
+    params.require(:contact).permit(:name, :email, :content)
+  end
+end
